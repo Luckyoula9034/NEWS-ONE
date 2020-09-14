1ca9db03c6e0e1f0bc6722b7a1cf3ada63112285
luqqy <you@example.com>
2020-09-14 07:39:38 +0300

    added a config.py file Where we will store our app configurations.

--- app/views.py
+++ app/views.py
@@ -2,11 +2,10 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<news_id')
-def index(news_id):
+@app.route('/news/<int:news_id>')
+def movie(news_id):
 
     '''
     View news page function that returns the news details page and its data
     '''
-
- return render_template('news.html',id = news_id)
+    return render_template('news.html',id = news_id)
