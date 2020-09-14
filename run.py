c3e8a84cb1af270a7d1fdea7248ebb4617844253
luqqy <you@example.com>
2020-09-14 07:18:09 +0300

    moved files to there respective folder to bring uniformity

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
- 
-@app.route('/')
-def index():
-    return '<h1> news@one </h1>'
-    
+from app import app
+
 if __name__ == '__main__':
-         app.run(debug = True)
+    app.run(debug = True)
